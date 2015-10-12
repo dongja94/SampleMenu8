2622cec4c56a4946ebcd949d2d6af4f64bc7dfac
dongja94 <dongja94@gmail.com>
2015-10-12 11:48:32 +0900

    ActionMode

--- app/src/main/java/com/example/dongja94/samplemenu/MainActivity.java
+++ app/src/main/java/com/example/dongja94/samplemenu/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.ShareActionProvider;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,9 +21,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        registerForContextMenu(findViewById(R.id.text_message));
+//        registerForContextMenu(findViewById(R.id.text_message));
         registerForContextMenu(findViewById(R.id.image_icon));
+        Button btn = (Button)findViewById(R.id.btn_action_mode);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showActionMode();
+            }
+        });
     }
+
+    private void showActionMode() {
+        ActionMode mode = startSupportActionMode(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                getMenuInflater().inflate(R.menu.menu_action_mode, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_mode_item1 :
+                    case R.id.menu_mode_item2 :
+                        Toast.makeText(MainActivity.this, "Mode Item Click : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        mode.finish();
+                        return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
+        });
+        mode.setTitle("Action Mode Menu");
+    }
+
+    public void onPopupMenu(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        popup.inflate(R.menu.menu_action_mode);
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_mode_item1 :
+                    case R.id.menu_mode_item2 :
+                        Toast.makeText(MainActivity.this, "Popup Item Click : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
+        popup.show();
+    }
+
 
     EditText keywordView;
 
